68ec9e41958a35d98518e55891b44133ef15d7c0
Tuosheng Jiao <tjiao@umass.edu>
2025-04-10 16:00:51 +0800

    update 62

---
+++ solutions/python/No_062_不同路径.py
@@ -0,0 +1,22 @@
+# LeetCode No.62 不同路径
+# 题目链接: https://leetcode.cn/problems/unique-paths/description/
+
+#方法一：动态规划
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[0] * n for _ in range(m)]
+        for i in range(m):
+            dp[i][0] = 1
+        for j in range(n):
+            dp[0][j] =1
+        for i in range(1,m):
+            for j in range(1,n):
+                dp[i][j] = dp[i-1][j] +dp[i][j-1]
+        return dp[m-1][n-1]
+
+        
+        
+        
+        
+            
+        
