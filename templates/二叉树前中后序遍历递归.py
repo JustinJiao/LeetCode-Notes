cba52b61ff3d433c264a6e2c5ed0c13224bc33e7
Tuosheng Jiao <tjiao@umass.edu>
2025-09-02 13:53:48 -0400

    update template

--- templates/二叉树前中后序遍历递归.py
+++ templates/二叉树前中后序遍历递归.py
@@ -49,3 +49,57 @@ class Solution:
 
         dfs(root)
         return res
+
+
+#前中后序遍历统一方法
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+        stack = []
+        if root: 
+            stack.append((root,False))
+        while stack:
+            node,visted = stack.pop()
+            if visted: #通过是否是第一次访问，如果不是第一次访问就加入result，说明子节点已经处理了
+                result.append(node.val)
+            else:
+                stack.append((node,True))
+                if node.right:
+                    stack.append((node.right,False))
+                if node.left:
+                    stack.append((node.left,False))
+        return result
+
+
+#层序遍历：
+#方法一：
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        from collections import deque
+        result = []
+        queue = deque()
+        if root:
+            queue.append(root)
+        while queue:
+            l = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                l.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            result.append(l)
+        return result
