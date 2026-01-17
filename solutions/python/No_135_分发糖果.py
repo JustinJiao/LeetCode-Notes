7737dff5b6f878e5735cd3bbe18b58ed5245885a
Tuosheng Jiao <tjiao@umass.edu>
2026-01-17 16:47:28 -0500

    update 135

--- solutions/python/No_135_分发糖果.py
+++ solutions/python/No_135_分发糖果.py
@@ -6,12 +6,17 @@ from typing import List
 
 class Solution:
     def candy(self, ratings: List[int]) -> int:
-        temp = [1] * len(ratings)
-        for i in range(1, len(ratings)):
+        temp1 = [1]* len(ratings)
+        for i in range(1,len(ratings)):
             if ratings[i] > ratings[i-1]:
-                temp[i] = temp[i-1] +1
+                temp1[i] = temp1[i-1] +1
+        temp2 = [1] * len(ratings)
         for i in range(len(ratings)-2,-1,-1):
             if ratings[i] > ratings[i+1]:
-                temp[i] = max(temp[i],temp[i+1] +1)
-        return sum(temp)
+                temp2[i] = temp2[i+1] +1
+        result =0
+        for i in range(len(ratings)):
+            num = max(temp1[i],temp2[i])
+            result += num
+        return result
         
