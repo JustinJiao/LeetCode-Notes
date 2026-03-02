dfb8f33c43f71a3a0461cf1decf85bec657d552d
Tuosheng Jiao <tjiao@umass.edu>
2026-03-02 14:51:11 -0500

    update

--- solutions/python/No_236_二叉树的最近公共祖先.py
+++ solutions/python/No_236_二叉树的最近公共祖先.py
@@ -3,28 +3,29 @@
 
 # Definition for a binary tree node.
 #方法一: 递归
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        def help(cur,p,q):
+        def help(cur):
             if not cur:
                 return None
-            if cur == p or  cur == q:
+            left = help(cur.left) #递归左子树
+            right = help(cur.right) #递归右子树
+            if cur == p or cur == q: #如果当前节点是p或者q,就返回当前节点
                 return cur
-            left = help(cur.left,p,q)
-            right = help(cur.right,p,q)
-
-            if left and right:
+            if left and right:#如果左子树和右子树都不为空,说明p和q分别在左右子树中,所以当前节点就是最近公共祖先
                 return cur
-
-            if left and not right:
+            if left:#如果左子树不为空,说明p和q都在左子树中,所以返回左子树的结果
                 return left
-            elif not left and right:
+            if right:#如果右子树不为空,说明p和q都在右子树中,所以返回右子树的结果
                 return right
-            else:
-                return None
-        return help(root,p,q)
+            return None
+        return help(root)
+            
+            
